b959949b664e42400aa126783035997b13640ac1
Mubarek Jemal <mubarekj71@gmail.com>
2026-05-11 12:32:51 +0300

    the getter, initialze the db and the table create

--- lib/services/expenses_db.dart
+++ lib/services/expenses_db.dart
@@ -8,4 +8,42 @@ class ExpensesDb {
   static final ExpensesDb instance = ExpensesDb._privateConstructor();
 
   Database? _database;
+
+  //database getter
+  Future<Database> get database async {
+    if (_database != null) return _database!;
+
+    _database = await _initDatabase();
+
+    return _database!;
+  }
+
+  //initialize databse
+  Future<Database> _initDatabase() async {
+    final dbPath = await getDatabasesPath();
+
+    final path = join(dbPath, 'expenses.db');
+
+    return await openDatabase(
+      path,
+      version: 1,
+      onCreate: _createDb,
+    );
+  }
+
+  //create table
+  Future<void> _createDb(
+  Database db,
+  int version,
+) async {
+  await db.execute('''
+    CREATE TABLE expenses(
+      id INTEGER PRIMARY KEY AUTOINCREMENT,
+      amount REAL NOT NULL,
+      category TEXT NOT NULL,
+      note TEXT NOT NULL,
+      date TEXT NOT NULL
+    )
+  ''');
+}
 }
