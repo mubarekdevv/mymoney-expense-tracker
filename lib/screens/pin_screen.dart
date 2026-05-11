7b9ae339fdf30849dff3180cf0ef4e9fe128ca47
Mubarek Jemal <mubarekj71@gmail.com>
2026-05-11 17:39:06 +0300

    PIN screen created without build method

---
+++ lib/screens/pin_screen.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+
+import '../services/pin_vault.dart';
+import 'expenses_screen.dart';
+
+class PinScreen extends StatefulWidget {
+  const PinScreen({super.key});
+
+  @override
+  State<PinScreen> createState() => _PinScreenState();
+}
+
+
+
+
+
+
+
